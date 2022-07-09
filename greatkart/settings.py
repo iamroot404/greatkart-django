7c4deac684de5be5ed6bbdcc568cd9b356bda8c2
iamroot404 <105676995+iamroot404@users.noreply.github.com>
2022-07-09 15:04:58 +0300

    Update settings.py

--- greatkart/settings.py
+++ greatkart/settings.py
@@ -149,5 +149,5 @@ MESSAGE_TAGS = {
 EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_PORT = 587
 EMAIL_HOST_USER = 'regmotech@gmail.com'
-EMAIL_HOST_PASSWORD = 'odbwmjqnasdyjmie'
+EMAIL_HOST_PASSWORD = ''
 EMAIL_USE_TLS = True
